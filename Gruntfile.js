75ca686a0eb27dd229b22867dad7f24336c3e20c
david <david.mazvovsky@gmail.com>
2014-02-08 05:13:56 +0200

    add fonts to build

--- Gruntfile.js
+++ Gruntfile.js
@@ -252,7 +252,9 @@ module.exports = function (grunt) {
             // Like Jekyll, exclude files & folders prefixed with an underscore.
             '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here.
-            '_bower_components/jquery/jquery.js',
+            '_bower_components/**/*.js',
+            '_bower_components/**/fonts/*',
+            '_bower_components/**/font/*',
             'favicon.ico',
             'apple-touch*.png'
           ],
